d4ea6da54cec8b8f971982a841f0796ea363889d
Forest Belton <65484+forestbelton@users.noreply.github.com>
2024-03-18 18:51:16 -0700

    Fix asset path for GitHub pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/ro-guild-quiz/",
   define: {
     __RAW_QUESTIONS_CSV__: JSON.stringify(
       fs.readFileSync("./src/questions.csv", { encoding: "utf-8" })
